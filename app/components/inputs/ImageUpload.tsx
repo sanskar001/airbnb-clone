0fa02faf09daced58081a4c9b13e55dd795d6d00
sanskar001 <sanskarm001@gmail.com>
2024-08-31 18:59:13 +0530

    feat: added Image upload with cloudinary CDN

---
+++ app/components/inputs/ImageUpload.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { FC } from "react";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useCallback } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
+
+declare global {
+  var cloudinary: any;
+}
+
+export interface ImageUploadProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+const ImageUpload: FC<ImageUploadProps> = ({ value, onChange }) => {
+  const handleUpload = useCallback(
+    (result: any) => {
+      onChange(result.info.secure_url);
+    },
+    [onChange]
+  );
+
+  return (
+    <CldUploadWidget
+      uploadPreset="first-upload-preset"
+      onSuccess={handleUpload}
+      options={{
+        maxFiles: 1,
+      }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className="relative cursor-pointer hover:opacity-70 transition boder-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600"
+          >
+            <TbPhotoPlus size={50} />
+            <div className="font-semibold text-lg">Click to upload</div>
+            {value && (
+              <div className="absolute inset-0 w-full h-full">
+                <Image
+                  alt="Upload"
+                  src={value}
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+            )}
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+};
+
+export default ImageUpload;
